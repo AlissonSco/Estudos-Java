a998b6ae3baa3702c52fbd52d1b486d23215aaba
Alisson <sousa.alisson07@aluno.ifce.edu.br>
2025-10-21 11:55:37 -0300

    Make properties private in Losangulo class
    Changed visibility of lado_maior, lado_menor, and area_losangulo to private.

--- FigurasPlanas/src/figuras/planas/Losangulo.java
+++ FigurasPlanas/src/figuras/planas/Losangulo.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Losangulo {
 	Scanner sc = new Scanner(System.in);
 	
-	public double lado_maior;
-	public double lado_menor;
-	public double area_losangulo;
+	private double lado_maior;
+	private double lado_menor;
+	private double area_losangulo;
 	
 	public void recebeLadoMenor() {
 		System.out.println("Digite o valor do lado menor do losangulo:");
@@ -32,3 +32,4 @@ public class Losangulo {
 		System.out.println("A área do losangulo de lado maior " +lado_maior+ " e lado menor " +lado_menor+ " é " +area_losangulo);
 	}
 }
+
